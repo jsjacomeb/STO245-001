b24da501bf17eedab77502d06f4ac681037ed23d
jsjacomeb <68928410+jsjacomeb@users.noreply.github.com>
2021-08-16 21:58:13 -0500

    Update Read csv.py

--- Proyecto/Codigo/Read csv.py
+++ Proyecto/Codigo/Read csv.py
@@ -1,5 +1,5 @@
 import os
-paths_to_folders = ['C:\\Users\Directorio', 'C:\\Users\Directorio']
+paths_to_folders = ['C:\\Users\Directorio\ganado_sano', 'C:\\Users\Directorio\ganado_enfermo']
 
 for folder in paths_to_folders:
    for csv_file in os.listdir(folder):
